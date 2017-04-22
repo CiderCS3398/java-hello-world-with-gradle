5a81bcb357a68d1109849fa8875501de336f43df
Sherman <stt19@txstate.edu>
2017-04-22 12:59:43 -0500

    Adding My Pass Test
    Adding my GreeterPass method to the TestGreeter.java file on my
    personal branch.

--- src/test/java/testHello/TestGreeter.java
+++ src/test/java/testHello/TestGreeter.java
@@ -30,4 +30,12 @@ public class TestGreeter {
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
    }
+
+   @Test
+   public void newTestSTGreeterPass() 
+   {
+      g.setName(" ");
+      assertEquals(g.getName()," ");
+      assertEquals(g.sayHello(),"Hello  !");
+   }
 }
