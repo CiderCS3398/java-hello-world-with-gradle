76c64cb800993aef66f316041c980a630198ee4e
nla19 <nla19@txstate.edu>
2017-04-23 14:50:51 -0500

    Test Successful (Hopefully)

--- src/test/java/testHello/TestGreeter.java
+++ src/test/java/testHello/TestGreeter.java
@@ -9,25 +9,36 @@ import org.junit.Test;
 public class TestGreeter {
 
    private Greeter g;
-  
+   private Greeter nick;
+
    @Before
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
    {
       g = new Greeter();
+      nick = new Greeter();
    }
 
    @Test
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
-  
+
    @Test
-   public void testGreeter() 
+   public void testGreeter()
    {
       g.setName("World");
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
+   }
+
+   @Test
+   public void newestNAGreeterTest(){
+      g.setName("Nick");
+      nick.setName(g.getName());
+
+      assertEquals(g.getName(), nick.getName());
+      assertEquals(g.sayHello(), "Hello Nick!");
    }
 }
