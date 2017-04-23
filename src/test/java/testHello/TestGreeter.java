3e5d81f433a93c16e78025a8787d1b8ebb35075e
forrest87 <forrestshort@me.com>
2017-04-23 09:31:08 -0500

    Add Testing for Non-Null Class Reference. Passes.

--- src/test/java/testHello/TestGreeter.java
+++ src/test/java/testHello/TestGreeter.java
@@ -9,25 +9,31 @@ import org.junit.Test;
 public class TestGreeter {
 
    private Greeter g;
-  
+
    @Before
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
    {
       g = new Greeter();
    }
 
    @Test
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
-  
+
    @Test
-   public void testGreeter() 
+   public void testGreeter()
    {
       g.setName("World");
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
+   }
+
+   @Test
+   public void newest_FS_GreeterPass()
+   {
+     assertNotNull(g);
    }
 }
